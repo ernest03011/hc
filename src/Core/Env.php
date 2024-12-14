113df618ccae71b6ca4381956363e8ba63c38c4a
Manuel De Jesus <manuel.ernestodjs@gmail.com>
2024-12-13 22:16:03 -0400

    Add SMTP and Social details and fix the get magic method which was missing an underscore

--- src/Core/Env.php
+++ src/Core/Env.php
@@ -12,15 +12,28 @@ class Env{
   {
     $this->config = [
       'smtp' => [
-
+        'name' => $env['SMTP_NAME'],
+        'debug' => $env['SMTP_DEBUG'],
+        'host' => $env['SMTP_HOST'],
+        'port' => $env['SMTP_PORT'],
+        'encryption' => $env['SMTP_SECURE'],
+        'auth' => $env['SMTP_AUTH'],
+        'username' => $env['SMTP_USERNAME'],
+        'password' => $env['SMTP_PASSWORD'],
+        'setFrom' => $env['SMTP_SET_FROM'],
+        'addReplyTo' => $env['SMTP_ADD_REPLY_TO'],
+        'ReceiverAddress' => $env['SMTP_RECEIVER_ADDRESS'],
+        
       ],
-      'social' => [
 
+      'social' => [
+        'ig' => $env['SOCIAL_IG_URL'],
+        'airbnb' => $env['SOCIAL_AIRBNB_URL']
       ]
     ];
   }
 
-  public function _get(string $name) : array | null
+  public function __get(string $name) : array | null
   {
     return $this->config[$name] ?? null;
   }
