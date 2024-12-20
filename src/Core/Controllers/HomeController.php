699ff09b00fd8975260236207b03b1301edfae18
Manuel De Jesus <manuel.ernestodjs@gmail.com>
2024-12-19 22:14:01 -0400

    Add the controller folder and create controller class for index and testing page

---
+++ src/Core/Controllers/HomeController.php
@@ -0,0 +1,17 @@
+<?php 
+
+declare(strict_types=1);
+
+namespace App\Core\Controllers;
+
+use App\Core\View;
+
+class HomeController{
+
+  public function index() : View
+
+  {
+    return View::make('index.view');
+  }
+
+}
