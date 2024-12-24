d32da9e6b113e8b9f7f9d66be3b05c794dfed07b
Manuel De Jesus <manuel.ernestodjs@gmail.com>
2024-12-24 01:55:59 -0400

    Start creating the class that will be used to validate data

---
+++ src/Core/Validator.php
@@ -0,0 +1,17 @@
+<?php 
+
+declare(strict_types=1);
+
+namespace App\Core;
+
+class Validator{
+
+  public function getValidationErrors(array $data) : array
+  {
+
+    $errors = [];
+
+    return $errors;
+  }
+
+}
