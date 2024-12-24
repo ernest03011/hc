b986764d4712da3bb0f60188a097ea6c2330bc77
Manuel De Jesus <manuel.ernestodjs@gmail.com>
2024-12-24 01:59:23 -0400

    Create a factory class to create factories for mailer used with phpmailer

---
+++ src/Factories/PHPMailerFactory.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Factories;
+
+use PHPMailer\PHPMailer\PHPMailer;
+
+class PHPMailerFactory{
+
+  public function createPHPMailer() : PHPMailer
+  {
+    $mailer = new PHPMailer(true);
+    return $mailer;
+  }
+}
