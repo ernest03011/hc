5aa9f5f915f6beb449b63d4720aaeed34726a567
Manuel De Jesus <manuel.ernestodjs@gmail.com>
2024-12-24 21:51:01 -0400

    Add the Env class to the dependecy container. Add routes for the contact form, for its view and form submission, and instantiate the request class to pass that object to the App class

--- public/index.php
+++ public/index.php
@@ -6,9 +6,11 @@ use App\Core\App;
 use App\Core\Env;
 use Dotenv\Dotenv;
 use App\Core\Router;
+use App\Core\Request;
 use App\Core\Container;
 use App\Core\Controllers\HomeController;
 use App\Core\Controllers\TestController;
+use App\Core\Controllers\ContactController;
 
 require_once __DIR__ . "/../vendor/autoload.php";
 
@@ -20,18 +22,26 @@ $router = new Router($container);
 
 $router
     ->get('/', [HomeController::class, 'index']);
+$router
+    ->get('/contact', [ContactController::class, 'index']);
+
+$router
+    ->post('/contact', [ContactController::class, 'sendEmail']);
 
 // $router
 //     ->get('/test', [TestController::class, 'index']);
+
+$container->set(Env::class, function () {
+    return new Env($_ENV);
+});
+
+$request = new Request($_GET, $_POST, $_SERVER);
 
 (new App(
 
     $container,
     $router,
-    [
-        'uri' => $_SERVER['REQUEST_URI'],
-        'method' => $_SERVER['REQUEST_METHOD']
-    ],
+    $request,
     new Env($_ENV)
 
 ))->run();
