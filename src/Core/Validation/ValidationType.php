94b186d397eff2cd1412c30fcd783aaffef86c58
Manuel De Jesus <manuel.ernestodjs@gmail.com>
2024-12-28 00:13:23 -0400

    Create enum with validationType. IT can grow as soon as new type of validations are needed

---
+++ src/Core/Validation/ValidationType.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Core\Validation;
+
+enum validationType : string 
+{
+  case REQUIRED = 'required';
+  case EMAIL = 'email';
+  case NUMERIC = 'numeric';
+  case MAX_LENGTH = 'max_length';
+}
