e365f843c3c3d2a9062726e1be9f41508d2542e7
Manuel De Jesus <manuel.ernestodjs@gmail.com>
2024-12-28 23:57:12 -0400

    Add a construct to pass the configuration class and then use it to get variables like storage path and the URLs for social apps

--- src/Core/Controllers/HomeController.php
+++ src/Core/Controllers/HomeController.php
@@ -4,14 +4,26 @@ declare(strict_types=1);
 
 namespace App\Core\Controllers;
 
+use App\Core\Env;
 use App\Core\View;
 
 class HomeController{
 
-  public function index() : View
+  public function __construct(
+    private Env $config
+  ) {
+  }
 
+  public function index() : View
   {
-    return View::make('index.view');
+    return View::make(
+      'index.view',
+      [
+        'storagePath' => $this->config->storage['path'],
+        'ig' => $this->config->social['ig'],
+        'airbnb' => $this->config->social['airbnb']
+      ]
+    );
   }
 
 }
