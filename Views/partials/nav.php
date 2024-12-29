1ac1b4ff64dff0bbef716e0318220272cf4720d3
Manuel De Jesus <manuel.ernestodjs@gmail.com>
2024-12-28 23:52:01 -0400

    Update the links inside the nav menu to use / instead of # and add more items, like gallery, about, activities, etc

--- Views/partials/nav.php
+++ Views/partials/nav.php
@@ -3,9 +3,10 @@
     <nav>
         <div class="logo">Countryside Stay</div>
         <ul>
-            <li><a href="#hero">Home</a></li>
-            <li><a href="#carousel-section">Gallery</a></li>
-            <li><a href="#activities">Activities</a></li>
-            <li><a href="#contact">Contact</a></li>
+            <li><a href="/">Home</a></li>
+            <li><a href="/gallery">Gallery</a></li>
+            <li><a href="/activities">Activities</a></li>
+            <li><a href="/contact">Contact</a></li>
+            <li><a href="/about">About</a></li>
         </ul>
     </nav>
