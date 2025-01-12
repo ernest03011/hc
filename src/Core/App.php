14ca1a80e2d24d7e39d97dcc90d87b6c14a2c04f
Manuel De Jesus <manuel.ernestodjs@gmail.com>
2025-01-11 22:39:12 -0400

    Refactor the class to add a boot method, and handle some of the settings to initiate the app like bindings to the container

--- src/Core/App.php
+++ src/Core/App.php
@@ -4,8 +4,9 @@ declare(strict_types=1);
 
 namespace App\Core;
 
-use App\Core\Validation\Validator;
+use Dotenv\Dotenv;
 use App\Core\ContactForm;
+use App\Core\Validation\Validator;
 use App\Interfaces\EmailInterface;
 use App\Interfaces\ValidatorInterface;
 use App\Exceptions\RouteNotFoundException;
@@ -13,21 +14,50 @@ use App\Exceptions\RouteNotFoundException;
 class App{
 
 
+  private Env $config;
+  private Request $request;
+
   public function __construct(
 
     protected Container $container,
-    protected Router $router,
-    protected Request $request,
-    protected Env $config
+    protected Router $router
   
   ) {
 
+    // $this->container->set(EmailInterface::class, ContactForm::class);
+    // $this->container->set(ValidatorInterface::class, Validator::class);
+
+    // View::setViewPath(
+    //   $config->view['path'] ?? __DIR__ . '/../views'
+    // );
+
+  }
+
+  public function boot(): static
+  {
+
+    $dotEnv = Dotenv::createImmutable(dirname(__DIR__, 2));
+    $dotEnv->load();
+
+    
     $this->container->set(EmailInterface::class, ContactForm::class);
     $this->container->set(ValidatorInterface::class, Validator::class);
+    $this->container->set(Env::class, function () {
+      return new Env($_ENV);
+    });
+    
+    $this->container->set(Request::class, function () {
+      return new Request($_GET, $_POST, $_SERVER);
+    });
+
+    $this->config = new Env($_ENV);
+    $this->request = new Request($_GET, $_POST, $_SERVER);
 
     View::setViewPath(
-      $config->view['path'] ?? __DIR__ . '/../views'
+      $this->config->view['path'] ?? __DIR__ . '/../views'
     );
+
+    return $this;
 
   }
 
