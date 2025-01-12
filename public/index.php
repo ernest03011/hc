e3e9e650c95e41ad5f5c86eeccc0ecd35d504ec5
Manuel De Jesus <manuel.ernestodjs@gmail.com>
2025-01-11 22:44:34 -0400

    Refactor it to start using the new Router method to register routes, and the new method to boot the App

--- public/index.php
+++ public/index.php
@@ -3,10 +3,7 @@
 declare(strict_types=1);
 
 use App\Core\App;
-use App\Core\Env;
-use Dotenv\Dotenv;
 use App\Core\Router;
-use App\Core\Request;
 use App\Core\Container;
 use App\Core\Controllers\HomeController;
 use App\Core\Controllers\TestController;
@@ -17,44 +14,25 @@ use App\Core\Controllers\GalleryController;
 
 require_once __DIR__ . "/../vendor/autoload.php";
 
-$dotEnv = Dotenv::createImmutable(dirname(__DIR__));
-$dotEnv->load();
 
 $container = new Container();
 $router = new Router($container);
 
-$router
-    ->get('/', [HomeController::class, 'index']);
+$router->registerRoutesFromControllerAttributes(
+    [
+        HomeController::class,
+        AboutController::class,
+        GalleryController::class,
+        ActivityController::class,
+        TestController::class,
+        ContactController::class
 
-$router
-    ->get('/about', [AboutController::class, 'index']);
-
-$router
-    ->get('/gallery', [GalleryController::class, 'index']);
-
-$router
-    ->get('/activities', [ActivityController::class, 'index']);
-
-$router
-    ->get('/contact', [ContactController::class, 'index']);
-
-$router
-    ->post('/contact', [ContactController::class, 'sendEmail']);
-
-// $router
-//     ->get('/test', [TestController::class, 'index']);
-
-$container->set(Env::class, function () {
-    return new Env($_ENV);
-});
-
-$request = new Request($_GET, $_POST, $_SERVER);
+    ]
+);
 
 (new App(
 
     $container,
-    $router,
-    $request,
-    new Env($_ENV)
+    $router
 
-))->run();
+))->boot()->run();
