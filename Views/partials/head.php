f83b4114c850b1ee01e028a6a2a7c10daea5aaf7
Manuel De Jesus <manuel.ernestodjs@gmail.com>
2025-01-11 22:11:56 -0400

    Add module type to the js script tag, and update the google captcha key

--- Views/partials/head.php
+++ Views/partials/head.php
@@ -6,5 +6,7 @@
     <title>Cozy Countryside Retreat</title>
     <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,600&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="./css/style.css">
+    <script src="/js/index.js" type="module"></script>
+    <script src="https://www.google.com/recaptcha/api.js?render=6LcbNzkpAAAAAPvT5x0b_m25lwwG9tKZfXqt5lbE"></script>
 </head>
 <body>
