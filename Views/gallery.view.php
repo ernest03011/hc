8106d84cf270445080ed59f75ea81ad4377f04ed
Manuel De Jesus <manuel.ernestodjs@gmail.com>
2025-01-11 22:08:45 -0400

    Remove js code

--- Views/gallery.view.php
+++ Views/gallery.view.php
@@ -4,6 +4,7 @@
 <section class="page-banner">
     <h1>Gallery</h1>
 </section>
+
 <div class="container">
     <h2>Explore Our Retreat</h2>
     <p>A glimpse of what awaits you.</p>
@@ -20,36 +21,5 @@
         </div>
     </div>
 </div>
-
-<script>
-    const prevBtn = document.querySelector('.carousel-button.prev');
-    const nextBtn = document.querySelector('.carousel-button.next');
-    const carouselContainer = document.querySelector('.carousel-container');
-    const slides = document.querySelectorAll('.carousel-slide');
-
-    let currentSlide = 0;
-    const totalSlides = slides.length;
-
-    function showSlide(index) {
-        if (index < 0) currentSlide = totalSlides - 1;
-        else if (index >= totalSlides) currentSlide = 0;
-        else currentSlide = index;
-        carouselContainer.style.transform = `translateX(-${currentSlide * 100}%)`;
-    }
-
-    prevBtn.addEventListener('click', () => {
-        showSlide(currentSlide - 1);
-    });
-
-    nextBtn.addEventListener('click', () => {
-        showSlide(currentSlide + 1);
-    });
-
-    // Auto-play (optional)
-    setInterval(() => {
-        showSlide(currentSlide + 1);
-    }, 5000);
-</script>
-
 
 <?php include 'partials/footer.php'; ?>
