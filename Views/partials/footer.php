8a5ad448927909da947e4609db8f88161a9960c9
Manuel De Jesus <manuel.ernestodjs@gmail.com>
2025-01-11 22:10:13 -0400

    Remove js code

--- Views/partials/footer.php
+++ Views/partials/footer.php
@@ -2,38 +2,5 @@
         &copy; 2024 Countryside Stay. All rights reserved.
     </footer>
 
-    <!-- reCAPTCHA script -->
-    <script src="https://www.google.com/recaptcha/api.js" async defer></script>
-
-    <!-- Carousel JS -->
-    <script>
-        const prevBtn = document.querySelector('.carousel-button.prev');
-        const nextBtn = document.querySelector('.carousel-button.next');
-        const carouselContainer = document.querySelector('.carousel-container');
-        const slides = document.querySelectorAll('.carousel-slide');
-
-        let currentSlide = 0;
-        const totalSlides = slides.length;
-
-        function showSlide(index) {
-            if (index < 0) currentSlide = totalSlides - 1;
-            else if (index >= totalSlides) currentSlide = 0;
-            else currentSlide = index;
-            carouselContainer.style.transform = `translateX(-${currentSlide * 100}%)`;
-        }
-
-        prevBtn.addEventListener('click', () => {
-            showSlide(currentSlide - 1);
-        });
-
-        nextBtn.addEventListener('click', () => {
-            showSlide(currentSlide + 1);
-        });
-
-        // Auto-play (optional)
-        setInterval(() => {
-            showSlide(currentSlide + 1);
-        }, 5000);
-    </script>
 </body>
 </html>
