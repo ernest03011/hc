05ca1700ba83d77480984380b9ab5c9c8c50286c
Manuel De Jesus <manuel.ernestodjs@gmail.com>
2025-01-23 01:06:05 -0400

    Add tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: [
+    "./Views/**/*.{php,js,jsx,ts,tsx}",
+    "./public/**/*.{php,js,jsx,ts,tsx}",
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
