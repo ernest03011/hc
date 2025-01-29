b554a2ee4a326608a23a4213333c37d93ab95b03
Manuel De Jesus <manuel.ernestodjs@gmail.com>
2025-01-29 01:14:52 -0400

    Add screen size for xs on tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        xs: "420px",
+      },
+    },
   },
   variants: {
     extend: {},
